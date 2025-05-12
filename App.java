78687bf0c88053739bc5f96904cf3c8d168199ef
karwowskasandra <karwowskasandra@gmail.com>
2025-05-12 09:21:03 +0200

    Cambios en archivo App.java

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Sandra Gabriela Karwowska");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("I.E.S San Andres");
+    System.out.println("Villaquilambre");
   }
   
 }
